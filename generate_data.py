20e3215bb2a464ab6360dd27ce55069976a9e250
hannasg22 <hannastammer22@gmail.com>
2024-05-20 10:33:11 +0200

    Created file to generate the data and get it from there.

---
+++ generate_data.py
@@ -0,0 +1,24 @@
+"""This module takes the values we want to apply in the model and saves
+them in a file deuteron_values.jsonl to later use them in our functions.
+"""
+
+import jsonlines
+
+# DATA FOR DEUTERON MODEL
+data_deuteron = [
+    {"Potential": "Central", "Depth_c": 13.888, "Range_c": 2.8},
+    {"Potential": "Tensor", "Depth_t": 10.7632, "Range_t": 2.8},
+    {"Initial conditions": "us wavefunction", "us_0": 0.0, "vs_0": 1.0},
+    {"Initial conditions": "ud wavefunction", "ud_0": 0.0, "vd_0": 0.03},
+    {"Boundary condition": "Final values", "us_fin": 0.0, "ud_fin": 0.0},
+    {"Range of radius": "Range r", "r_initial": 0.1, "r_final": 10.0},
+    {"Energy guess": -1.5}
+]
+
+file_name = "deuteron_values.jsonl"
+
+# WRITE VALUES IN THE FILE
+with jsonlines.open(file_name, mode='w') as writer:
+    writer.write_all(data_deuteron)
+
+print(f"'{file_name}' file has been created with the deuteron data in JSON lines format.")
