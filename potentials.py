060539d66ebb05a3754d25d8b2e350279665f79e
hannasg22 <hannastammer22@gmail.com>
2024-05-20 11:50:37 +0200

    Created file for potentials we will use in our model.

---
+++ potentials.py
@@ -0,0 +1,49 @@
+"""This module contains the potentials used to solve our equations.
+
+Parameters needed in the potentials:
+    r0: r value for the range of the square well potential (units: fm)
+    V0 : depth of the potential (units: MeV)
+These values have been chosen due to experimental data.
+
+For further information check README.md section.
+"""
+
+import jsonlines
+import get_values as get
+
+
+def V_c_squarewell(r):
+    """This function calculates the value of the central part of the
+    potential.         
+
+    Input:
+        r : distance between proton and neutron
+    Output:
+        Value of the central potential for a certain r
+    """
+
+    # Inserting values for central potential
+    V0_c = get.central_V()[0]
+    r0_c = get.central_V()[1]
+    if r <= r0_c:
+        return -V0_c
+    else:
+        return 0.0
+
+def V_t_squarewell(r):
+    """This function calculates the value of the tensorial part of the
+    potential for distinct values of r.
+
+    Input:
+        r : distance between proton and neutron
+    Output:
+        Value of the tensor potential for a certain r
+    """
+
+    # Inserting values for tensor potential
+    V0_t = get.tensorial_V()[0]
+    r0_t = get.tensorial_V()[1]
+    if r <= r0_t:
+        return -V0_t
+    else:
+        return 0.0
