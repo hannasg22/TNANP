f74d1cd3d7176ab080d26330e31215345dca2910
hannasg22 <hannastammer22@gmail.com>
2024-05-21 22:57:24 +0200

    Changed files to try to use least_squares in order to find E value. Still needs to be developed.

--- find_eigenvalue.py
+++ find_eigenvalue.py
@@ -18,7 +18,7 @@ import get_values as get
 
 def error_E(E_guess):
     """This function analyses if the results obtained with E_guess
-    matche the desired boundary conditions.
+    match the desired boundary conditions.
 
     We solve the system of equations both forwards and backwards with
     E_guess and compare the results in the midpoint called "cut".
@@ -29,26 +29,45 @@ def error_E(E_guess):
         error: difference between the results with E_guess in cut point
     """
 
+    E_insert = E_guess[0]
+    print(f"Running error_E with E_guess: {E_insert}")
+
     # Define the midpoint
     cut = get.range_of_radius()[1] * 0.9
+    print(f"cut: {cut}")
+
     # Get the conditions from the data file
     ini_cond = get.initial_conditions()
     fin_cond = get.boundary_conditions()
+    print(f"Initial conditions: {ini_cond}")
+    print(f"Boundary conditions: {fin_cond}")
+
     # Ranges for the forward and backward integration
     r_range1 = [get.range_of_radius()[0], cut]
     r_range2 = [get.range_of_radius()[1], cut]
-    
+    print(f"r_range1: {r_range1}")
+    print(f"r_range2: {r_range2}")
+
     # Solve system for E_guess forwards
-    sol1 = solve_ivp(lambda r, y: eq.radial_equations(r, y, E_guess),
+    sol1 = solve_ivp(lambda r, y: eq.radial_equations(r, y, E_insert),
                      r_range1, ini_cond, method='RK45', max_step=0.01)
     # Solve system for E_guess backwards
-    sol2 = solve_ivp(lambda r, y: eq.radial_equations(r, y, E_guess),
+    sol2 = solve_ivp(lambda r, y: eq.radial_equations(r, y, E_insert),
                      r_range2, fin_cond, method='RK45', max_step=0.01)
-     
+    
+    # Print solutions to check shapes and values
+    print(f"sol1.t: {sol1.t}")
+    print(f"sol1.y: {sol1.y}")
+    print(f"sol2.t: {sol2.t}")
+    print(f"sol2.y: {sol2.y}")
+
     # Result in midpoint "cut" for us, vs, ud, vd
     error_us = sol1.y[0][-1] - sol2.y[0][-1]
     error_vs = sol1.y[1][-1] - sol2.y[1][-1]
     error_ud = sol1.y[2][-1] - sol2.y[2][-1]
     error_vd = sol1.y[3][-1] - sol2.y[3][-1]
+
+    error = [error_us, error_vs, error_ud, error_vd]
+    print(f"error: {error}")
     
-    return [error_us, error_vs, error_ud, error_vd]
+    return error
--- get_solution.py
+++ get_solution.py
@@ -3,28 +3,16 @@
 """
 
 import numpy as np
-from scipy.optimize import root_scalar
-from scipy.optimize import root
+from scipy.optimize import root_scalar, root, least_squares
 from scipy.integrate import solve_ivp
 
 import find_eigenvalue as find
 import equations as eq
 import get_values as get
 
-# Get our real boundary conditions
-bound_cond = get.boundary_conditions()
-
 # Try to reach the E eigenvalue
 # What method should we use? root? root_scalar?
 
-"""
-sol = root_scalar(find.error_E, method='secant', x0=get.energy_guess())
-E_final = sol.root
-
-print(f"El valor de E que cumple las condiciones de contorno es: {E_final}")
-
-# Solve with the obtained E
-final = solve_ivp(lambda r, y: eq.radial_equations(r, y, E_guess),
-                  get.range_of_radius(), get.initial_conditions(),
-                  method='RK45', max_step=0.01)
-"""
+x0 = [get.energy_guess()]
+sol = least_squares(find.error_E, x0)
+print(f"Solution: {sol}")
