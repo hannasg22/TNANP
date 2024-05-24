e1268784eae630281c7c57aa5e01faf08c0d483a
hannasg22 <hannastammer22@gmail.com>
2024-05-24 15:00:37 +0200

    Added two linearly independent solutions to continue with d) section of gezerlis.

--- generate_data.py
+++ generate_data.py
@@ -8,12 +8,16 @@ import jsonlines
 data_deuteron = [
     {"Potential": "Central", "Depth_c": 13.888, "Range_c": 2.8},
     {"Potential": "Tensor", "Depth_t": 10.7632, "Range_t": 2.8},
-    {"Initial conditions": "us wavefunction", "us_0": 0.5, "vs_0": 1.0},
-    {"Initial conditions": "ud wavefunction", "ud_0": 0.125, "vd_0": 0.5},
-    {"Boundary conditions": "us wavefunction", "us_fin": 0.038, "vs_fin": -0.012},
-    {"Boundary conditions": "Derivatives", "ud_fin": 0.00038, "vd_fin": -0.0001},
+    {"Initial conditions A": "us wavefunction", "us_0": 0.5, "vs_0": 1.0},
+    {"Initial conditions A": "ud wavefunction", "ud_0": 0.125, "vd_0": 0.5},
+    {"Initial conditions B": "us wavefunction", "us_0": 0.4, "vs_0": 0.95},
+    {"Initial conditions B": "ud wavefunction", "ud_0": 0.1, "vd_0": 0.45},
+    {"Boundary conditions C": "us wavefunction", "us_fin": 0.038, "vs_fin": -0.012},
+    {"Boundary conditions C": "Derivatives", "ud_fin": 0.00038, "vd_fin": -0.0001},
+    {"Boundary conditions D": "us wavefunction", "us_fin": 0.036, "vs_fin": -0.01},
+    {"Boundary conditions D": "Derivatives", "ud_fin": 0.00036, "vd_fin": -0.00008},
     {"Range of radius": "Range r", "r_initial": 0.5, "r_final": 10.0},
-    {"Energy guess": -2.225}
+    {"Energy guess": -2.1}
 ]
 
 file_name = "deuteron_values.jsonl"
