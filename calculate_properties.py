08af04c76f89c14ddfb1841909a250a87fc58ab4
hannasg22 <hannastammer22@gmail.com>
2024-05-25 11:53:21 +0200

    Obtained the probability for ud.

--- calculate_properties.py
+++ calculate_properties.py
@@ -11,7 +11,6 @@ import find_results as find
 import equations as eq
 import get_values as get
 import get_graph as graph
-import get_solutions as solut
 
 def normalize():
     """This function will normalize our function to later get the proper
@@ -20,21 +19,13 @@ def normalize():
     Output:
         Normalization constant
     """
-    # Calculate proper E value
-    E_get = root_scalar(find.error_E, args=(solut.get_ABCD()[0],
-                                            solut.get_ABCD()[1],
-                                            solut.get_ABCD()[2],
-                                            solut.get_ABCD()[3]),
-                        method='secant', x0=get.energy_guess(),
-                        x1=get.energy_guess()+1.0)
-    E_fin = E_get.root
+    # Take values from data file
+    A, B, C, D = get.ABCD()
+    E = get.eigenvalue()
+    
+    # Solve system with proper variables
+    wavefs = graph.plot_functions(E, A, B, C, D)
 
-    # Solve system with proper E value
-    wavefs = graph.plot_functions(E_fin,
-                                  solut.get_ABCD()[0],
-                                  solut.get_ABCD()[1],
-                                  solut.get_ABCD()[2],
-                                  solut.get_ABCD()[3])
     # Get all values
     r_values_out = wavefs[4]
     r_values_in = wavefs[5]
@@ -67,22 +58,14 @@ def ud_probability():
     Output:
         ud probability
     """
-    # Calculate proper E value
-    E_get = root_scalar(find.error_E, args=(solut.get_ABCD()[0],
-                                            solut.get_ABCD()[1],
-                                            solut.get_ABCD()[2],
-                                            solut.get_ABCD()[3]),
-                        method='secant', x0=get.energy_guess(),
-                        x1=get.energy_guess()+1.0)
-    E_fin = E_get.root
+    # Take values from data file
+    A, B, C, D = get.ABCD()
+    E = get.eigenvalue()
 
     # Solve system with proper E value
-    wavefs = graph.plot_functions(E_fin,
-                                  solut.get_ABCD()[0],
-                                  solut.get_ABCD()[1],
-                                  solut.get_ABCD()[2],
-                                  solut.get_ABCD()[3])
-    # Get all values
+    wavefs = graph.plot_functions(E, A, B, C, D)
+    
+    # Get necessary values
     r_values_out = wavefs[4]
     r_values_in = wavefs[5]
     ud_values_out = wavefs[2]
@@ -92,11 +75,11 @@ def ud_probability():
     r_in_reverse = r_values_in[::-1]
     ud_in_reverse = ud_values_in[::-1]
 
-    # Unify all the results FOR NORMALIZATION
+    # Unify all the results
     r_values = np.concatenate((r_values_out, r_in_reverse))
     ud_values = np.concatenate((ud_values_out, ud_in_reverse))
 
-    ud_normlzd = normalize() * ud_values
+    ud_normlzd = ud_values / normalize()
 
     P_ud = np.trapz(ud_normlzd**2, r_values)
 
