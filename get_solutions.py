58c6fd301e12cc45dc037e34302d026090a20414
hannasg22 <hannastammer22@gmail.com>
2024-05-25 11:00:37 +0200

    We have improved as much as possible the obtained graphs.

--- get_solutions.py
+++ get_solutions.py
@@ -20,21 +20,16 @@ def get_ABCD():
     ABCD_values = root(find.error_ABCD, initial_ABCD)
     return ABCD_values.x
 
-A_fin = get_ABCD()[0]
-B_fin = get_ABCD()[1]
-C_fin = get_ABCD()[2]
-D_fin = get_ABCD()[3]
-
-print(get_ABCD())
-
-E_final = root_scalar(find.error_E, args=(A_fin, B_fin, C_fin, D_fin),
+E_final = root_scalar(find.error_E, args=(get_ABCD()[0], get_ABCD()[1],
+                                          get_ABCD()[2], get_ABCD()[3]),
                       method='secant', x0=get.energy_guess(),
                       x1=get.energy_guess()+1.0)
 
 E_use = E_final.root
 print(E_use)
 
-wf_final = graph.plot_functions(E_use, A_fin, B_fin, C_fin, D_fin)
+wf_final = graph.plot_functions(E_use, get_ABCD()[0], get_ABCD()[1],
+                                get_ABCD()[2], get_ABCD()[3])
 
 # Get the values for r, us and ud
 r_values_out = wf_final[4]
@@ -49,11 +44,12 @@ r_in_reverse = r_values_in[::-1]
 us_in_reverse = us_values_in[::-1]
 ud_in_reverse = ud_values_in[::-1]
 
-# Unify all the results
+# Unify all the results FOR THE IMAGE
 r_values = np.concatenate((r_values_out, r_in_reverse[50:]))
 us_values = np.concatenate((us_values_out, us_in_reverse[50:]))
 ud_values = np.concatenate((ud_values_out, ud_in_reverse[50:]))
 
+# Graphic settings
 sns.set(style='darkgrid')
 
 plt.figure(figsize=(10, 6))
@@ -74,31 +70,3 @@ plt.gca().tick_params(axis='x', colors='black')
 plt.gca().tick_params(axis='y', colors='black')
 
 plt.show()
-
-
-"""
-# Graph with seaborn
-sns.set(style='darkgrid')
-
-plt.figure(figsize=(10, 6))
-
-plt.style.use('dark_background')
-
-plt.plot(r_values_out, us_values_out, label='$u_s(r)$', color='pink')
-plt.plot(r_values_in, us_values_in, color='pink')
-plt.plot(r_values_out, ud_values_out, label='$u_d(r)$', color='magenta')
-plt.plot(r_values_in, ud_values_in, color='magenta')
-
-plt.xlabel('r', color='white')
-plt.ylabel('V(r)', color='white')
-plt.title('Eigenfunctions $u_s(r)$ and $u_d(r)$ vs. $r (fm)$', color='black')
-plt.legend()
-plt.grid(True, color='gray')
-
-plt.gca().spines['bottom'].set_color('black')
-plt.gca().spines['left'].set_color('black')
-plt.gca().tick_params(axis='x', colors='black')
-plt.gca().tick_params(axis='y', colors='black')
-
-plt.show()
-"""
