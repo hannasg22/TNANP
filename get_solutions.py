30b4e30436f7e2d464476633b2d7313f348c8d7a
hannasg22 <hannastammer22@gmail.com>
2024-05-26 19:27:44 +0200

    Checked documentation and correctness of code.

--- get_solutions.py
+++ get_solutions.py
@@ -1,6 +1,5 @@
 """This file contains the part in which we simply try to get the results
 for the energy and the A,B,C,D variables.
-
 """
 
 import numpy as np
@@ -11,18 +10,29 @@ import find_results as find
 import get_values as get
 import get_graph as graph
 
+
 # Find solution for ABCD coefficients
 def get_ABCD():
+    """This function takes initial guesses for A, B, C and D and solves
+    the system through a root finding method to reach variables which
+    obey the continuity conditions we require.
+
+    Output:
+        Proper result for A, B, C and D
+    """
+    
     initial_ABCD=[1., 1., 1., 1.]
     ABCD_values = root(find.error_ABCD, initial_ABCD)
     return ABCD_values.x
 
 A_get, B_get, C_get, D_get = get_ABCD()
 
+# Calculate E value with the last continuity condition
 E_final = root_scalar(find.error_E, args=(A_get, B_get, C_get, D_get),
                       method='secant', x0=get.energy_guess(),
                       x1=get.energy_guess()+1.0)
 
+# Check that the energy is the expected one
 E_use = E_final.root
 print(E_use)
 
