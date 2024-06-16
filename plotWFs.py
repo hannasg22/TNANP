63d40119b2868eab3be53dcf22069c5ca38c4e88
hannasg22 <hannastammer22@gmail.com>
2024-06-16 12:38:36 +0200

    Improving file so that the graph will be better.

--- plotWFs.py
+++ plotWFs.py
@@ -15,19 +15,19 @@ wf_final = graph.plot_functions(get.eigenvalue(), get.ABCD()[0],
                                 get.ABCD()[3])
 
 # Get the values for r, us and ud
-r_values_out = wf_final[4]
-r_values_in = wf_final[5]
 us_values_out = wf_final[0]
 us_values_in = wf_final[1]
 ud_values_out = wf_final[2]
 ud_values_in = wf_final[3]
+r_values_out = wf_final[4]
+r_values_in = wf_final[5]
 
 # Reverse order for arrays created inwards
 r_in_reverse = r_values_in[::-1]
 us_in_reverse = us_values_in[::-1]
 ud_in_reverse = ud_values_in[::-1]
 
-# Unify all the results FOR THE IMAGE
+# Unify all the results to generate the image
 r_values = np.concatenate((r_values_out, r_in_reverse[50:]))
 us_values = np.concatenate((us_values_out, us_in_reverse[50:]))
 ud_values = np.concatenate((ud_values_out, ud_in_reverse[50:]))
@@ -42,7 +42,7 @@ plt.style.use('dark_background')
 plt.plot(r_values, us_values, label='$u_s(r)$', color='pink')
 plt.plot(r_values, ud_values, label='$u_d(r)$', color='magenta')
 
-plt.xlabel('r', color='white')
+plt.xlabel('r (fm)', color='black')
 plt.title('Eigenfunctions $u_s(r)$ and $u_d(r)$ vs. $r (fm)$', color='black')
 plt.legend()
 plt.grid(True, color='gray')
